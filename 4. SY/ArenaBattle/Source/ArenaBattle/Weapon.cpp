8a991bc3c02f1a5fedb449bf26cc8c14f776606b
llssyy404 <christian404@naver.com>
2017-03-09 19:12:25 +0900

    이득우 블로그 C++ 언리얼 - [1-8]액터의 제작
    Signed-off-by: llssyy404 <christian404@naver.com>

---
+++ 4. SY/ArenaBattle/Source/ArenaBattle/Weapon.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ArenaBattle.h"
+#include "Weapon.h"
+
+
+// Sets default values
+AWeapon::AWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponHashValue"));
+	RootComponent = Weapon;
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> 
+		SK_BlackKnight(TEXT("SkeletalMesh'/Game/InfinityBladeWeapons/Weapons/Blade/Swords/Blade_BlackKnight/SK_Blade_BlackKnight.SK_Blade_BlackKnight'"));
+	Weapon->SetSkeletalMesh(SK_BlackKnight.Object);
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeapon::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
